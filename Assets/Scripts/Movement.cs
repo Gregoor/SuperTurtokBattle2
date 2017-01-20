446d2e0b2c1d1eb9ddcb91d2e7166e3980722afa
Gregor <gregorwbr@gmail.com>
2017-01-20 23:03:33 +0100

    simple player movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] float speed = 500;
+
+	void Update ()
+	{
+	    GetComponent<CharacterController>()
+	        .SimpleMove((
+	                        Vector3.right * Input.GetAxis("Horizontal")
+	                        + Vector3.forward * Input.GetAxis("Vertical")
+	                    ) * Time.fixedDeltaTime * speed);
+	}
+}
