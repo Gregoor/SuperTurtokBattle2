abb7c12537973e6ecfe4955ae11b075b43df001b
Marco <Marco>
2017-01-21 11:13:00 +0100

    First proper waves being shot

---
+++ Assets/Scripts/FireWave.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWave : MonoBehaviour {
+
+    public string waveOneButton;
+
+    public GameObject waveOne;
+
+    public Transform waveSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(waveOneButton))
+        {
+
+            //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            //transform.Rotate(0, x, 0);
+            //transform.Translate(0, 0, z);
+
+            //Instantiate(waveOne, transform.position, transform.rotation);
+            var wave = (GameObject)Instantiate(waveOne,waveSpawn.position,waveSpawn.rotation);
+
+            wave.GetComponent<Rigidbody>().velocity = wave.transform.forward * 6;
+
+            Destroy(wave, 10.0f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/WaveOneProperties.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveOneProperties : MonoBehaviour {
+
+    private float FlightSpeed = 10;
+
+    private float FlightLength = 3;
+
+    public GameObject Wave;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Wave.transform.Rotate(Vector3.up * 90);
+        Rigidbody WaveRigidBody;
+        WaveRigidBody = Wave.GetComponent<Rigidbody>();
+        WaveRigidBody.AddForce(transform.forward * FlightSpeed);
+        //WaveRigidBody.velocity = (transform.forward * FlightSpeed);
+        Destroy(Wave, FlightLength);
+    }
+}
